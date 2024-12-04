5d31e48a607090fb7f748ba700b0e534fbd683ea
Andrii <bilyjandrij4@gmail.com>
2024-12-04 14:17:36 +0200

    Resolved linter issues

--- laba/src/main/java/com/example/database/AuthorizationAdapter.java
+++ laba/src/main/java/com/example/database/AuthorizationAdapter.java
@@ -3,8 +3,8 @@ package com.example.database;
 public class AuthorizationAdapter {
     private Авторизація auth;
 
-    public AuthorizationAdapter(Авторизація авторизація) {
-        this.auth = авторизація;
+    public AuthorizationAdapter(Авторизація auth) {
+        this.auth = auth;
     }
     public boolean authorize(БазаДаних db) {
         return auth.авторизуватися(db);
